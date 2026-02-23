2be089b51d54162407445bd2980ddb91914bfdde
exec zsh <wind10cloud@gmail.com>
2026-02-23 10:35:47 +0900

    added serializers

---
+++ crm/serializers.py
@@ -0,0 +1,37 @@
+from rest_framework import serializers
+from .models import Deal, Activity
+
+
+class DealSerialier(serializers.ModelSerializer):
+    class Meta:
+        model = Deal
+        fields = [
+            "id",
+            "deal_name",
+            "client_name",
+            "status",
+            "next_follow_up_at",
+            "created_at",
+            "updated_at",
+        ]
+        read_only_fields = ["id", "created_at", "updated_at"]
+
+class ActivitySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Activity
+        fields = [
+            "id",
+            "deal",
+            "activity_type",
+            "date",
+            "memo",
+            "created_at",
+            "updated_at",
+        ]
+        read_only_fields = ["id", "created_at", "updated_at"]
+
+class ActivityCreateSerializer(serializers.ModelSerializer):
+    """/deals/{id}/activities の POST 用（dealはURLから決める）"""
+    class Meta:
+        model = Activity
+        fields = ["date", "activity_type", "memo"]
